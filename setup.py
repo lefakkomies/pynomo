d329e05eaae4a4ef71bf0c49e1338b4dd2e8f549
Leif Roschier <leif.roschier@iki.fi>
2023-03-19 21:02:12 +0200

    year

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 #
 #    PyNomo - nomographs with Python
-#    Copyright (C) 2007- 2020  Leif Roschier
+#    Copyright (C) 2007- 2023  Leif Roschier
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
